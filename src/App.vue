74eed72378b6b06b1e30aa6cf6671500c1fc71cb
Farih Muhammad <farihul.ibad@gmail.com>
2021-02-17 11:17:05 +0700

    init vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Deck />
+  </div>
+</template>
+
+<script>
+import Deck from "./components/Deck.vue";
+
+export default {
+  name: "App",
+  components: {
+    Deck,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Deck.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="deck-page">
+    <h1>Hello Deck</h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: "Deck",
+  props: {},
+  data: function () {
+    return {
+      data: false,
+    };
+  },
+  created: function () {
+    console.log("this is created");
+    Vue.axios.get('https://swapi.dev/api/starships').then((response) => {
+      console.log(response.data);
+    });
+  },
+  methods: {
+    fetchData() {},
+  },
+};
+</script>
+
+<style>
+</style>
